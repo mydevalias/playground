68ba05d99a1268858303321db6b202ff97ab4af4
liviu <liviu31@gmail.com>
2014-03-06 22:34:06 +0200

    raw queue

---
+++ src/com/playground/aads/RawQueue.java
@@ -0,0 +1,42 @@
+package com.playground.aads;
+
+/**
+ * User: liviu
+ * Date: 3/4/14
+ * Time: 9:34 PM
+ */
+public class RawQueue<T> {
+
+    Node<T> head, tail;
+
+    public void enqueue(T data) {
+        if (head == null) {
+            head = new Node(data);
+            tail = head;
+        } else {
+            tail.next = new Node(data);
+            tail = tail.next;
+        }
+    }
+
+    public T dequeue() {
+        if (head != null) {
+            T data = head.data;
+            head = head.next;
+            if (head == null) {
+                tail = null;
+            }
+            return data;
+        }
+        return null;
+    }
+
+    public class Node<T> {
+        T data;
+        Node next;
+
+        public Node(T data) {
+            this.data = data;
+        }
+    }
+}
