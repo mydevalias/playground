0b54d14ca7fe2ee8cd9d8ae95ca32850478bb516
liviu <liviu31@gmail.com>
2014-03-18 23:02:28 +0200

    basic table map without resize

---
+++ src/com/playground/aads/RawTableMap.java
@@ -0,0 +1,67 @@
+package com.playground.aads;
+
+/**
+ * User: liviu
+ * Date: 3/5/14
+ * Time: 11:11 PM
+ */
+public class RawTableMap<K, V> {
+
+    private Entry<K, V> table[];
+
+    public RawTableMap() {
+        table = new Entry[15];
+    }
+
+    public void put(K key, V value) {
+        int place = row(key);
+        if (table[place] == null) {
+            table[place] = new Entry<>(key, value);
+        } else {
+            Entry<K, V> entry = table[place];
+            while (true) {
+                if (entry.key.equals(key)) {
+                    entry.data = value;
+                    return;
+                } else {
+                    if (entry.next != null) {
+                        entry = entry.next;
+                    } else {
+                        entry.next = new Entry<>(key, value);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    public V get(K key) {
+        int place = row(key);
+        Entry<K, V> entry = table[place];
+        while (entry != null) {
+            if (entry.key.equals(key)) {
+                return entry.data;
+            } else {
+                entry = entry.next;
+            }
+        }
+        return null;
+    }
+
+    private int row(K key) {
+        return key.hashCode() % table.length;
+    }
+
+
+    public class Entry<K, V> {
+        K key;
+        V data;
+        Entry<K, V> next;
+
+        public Entry(K key, V data) {
+            this.key = key;
+            this.data = data;
+        }
+    }
+
+}
