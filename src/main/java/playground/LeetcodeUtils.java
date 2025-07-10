10c3ce10547c635d297e9d4aa41963a32b5c8f33
Liviu <liviu31@gmail.com>
2025-07-10 19:20:46 +0300

    fast method for checking a string is a palindrome

--- src/main/java/playground/LeetcodeUtils.java
+++ src/main/java/playground/LeetcodeUtils.java
@@ -19,6 +19,25 @@ public class LeetcodeUtils {
         return counter;
     }
 
+    public static boolean isPalindrome(String s) {
+        if (s == null || s.length() <= 1) {
+            return true;
+        }
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
     public static class BooleanSparseMatrix{
         private final int rows;
         private final int cols;
