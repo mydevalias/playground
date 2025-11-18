d13efa8ada342c6071a5f036074f3ba0d30aa58c
Liviu Stirb <liviu31@gmail.com>
2025-11-18 19:10:51 +0200

    leetcode problem

---
+++ src/main/java/playground/leetcode/CountOperationsObtainZero.java
@@ -0,0 +1,30 @@
+package playground.leetcode;
+
+public class CountOperationsObtainZero {
+
+
+    public int countOperations(int num1, int num2) {
+        int count = 0;
+        while (num1 != 0 && num2 != 0) {
+            if (num1 >= num2) {
+                num1 = num1 - num2;
+            } else {
+                num2 = num2 - num1;
+            }
+            count++;
+        }
+        return count;
+    }
+
+    public int countOperations2(int num1, int num2) {
+        if (num1 == 0 || num2 == 0) {
+            return 0;
+        }
+        if (num1 >= num2) {
+            return 1 + countOperations(num1 - num2, num2);
+        }
+        return 1 + countOperations(num1, num2 - num1);
+    }
+
+
+}
