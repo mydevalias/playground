881f275509bdfa2b954a746cb5a0df2345504e72
Liviu Stirb <liviu31@gmail.com>
2025-12-31 12:37:14 +0200

    stuff

---
+++ src/main/java/playground/leetcode/CountNegativeNumbersInSortedMatrix.java
@@ -0,0 +1,15 @@
+package playground.leetcode;
+
+public class CountNegativeNumbersInSortedMatrix {
+    public int countNegatives(int[][] grid) {
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if(grid[i][j]<0){
+                     res++;
+                }
+            }
+        }
+        return res;
+    }
+}
