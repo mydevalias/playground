0a9b3c8d3d4a137817d90be357dafafdf540b936
Liviu Stirb <liviu31@gmail.com>
2026-03-03 19:09:28 +0200

    leetcode problem

---
+++ src/main/java/playground/leetcode/PartitioningIntoMinimumNumberOfDeciBinaryNumbers.java
@@ -0,0 +1,14 @@
+package playground.leetcode;
+
+public class PartitioningIntoMinimumNumberOfDeciBinaryNumbers {
+
+    public int minPartitions(String n) {
+        for (char ch = '9'; ch >= '0'; ch--) {
+            if (n.indexOf(ch) > -1) {
+                return ch - '0';
+            }
+        }
+        return -1;
+    }
+
+}
