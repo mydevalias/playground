84ba9416da5bfd50f2d1dde3848c27e1f22d02a3
Liviu Stirb <liviu31@gmail.com>
2026-05-26 13:31:08 +0300

    leetcode problem

--- src/main/java/playground/leetcode2/JumpGameVII.java
+++ src/main/java/playground/leetcode2/JumpGameVII.java
@@ -7,30 +7,28 @@ import java.util.Queue;
 public class JumpGameVII {
 
     public boolean canReach(String s, int minJump, int maxJump) {
-        if (s.charAt(s.length() - 1) != 0) {
+        if (s.charAt(s.length() - 1) != '0') {
             return false;
         }
         BitSet visited = new BitSet(s.length());
-        Queue<Integer> stack = new LinkedList<>();
-        stack.add(0);
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
         visited.set(0);
+        int maxReached = 0;
 
-        while (!stack.isEmpty()) {
-            int pos = stack.poll();
-            for (int i = Math.min(pos + minJump, s.length()); i < Math.min(pos + maxJump, s.length()); i++) {
-                if (visited.get(i)) {
-                    continue;
-                }
+        while (!queue.isEmpty()) {
+            int pos = queue.poll();
+            for (int i = Math.max(pos + minJump, maxReached + 1); i <= Math.min(pos + maxJump, s.length() - 1); i++) {
                 if (s.charAt(i) == '0') {
                     if (i == s.length() - 1) {
                         return true;
                     }
                     visited.set(i);
-                    stack.add(i);
+                    queue.add(i);
                 }
+                maxReached = i; //this will help to skip already visited on I don't get O(n^2)
             }
         }
         return false;
     }
-
 }
