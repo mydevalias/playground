23e4423fbfc12a3dfe12d6515129196091354b61
Liviu Stirb <liviu31@gmail.com>
2026-06-29 23:22:39 +0300

    leetcode problem

---
+++ src/main/java/playground/leetcode2/MaximumElementAfterDecreasingandRearranging.java
@@ -0,0 +1,17 @@
+package playground.leetcode2;
+
+import java.util.Arrays;
+
+public class MaximumElementAfterDecreasingandRearranging {
+
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        Arrays.sort(arr);
+        arr[0] = 1;
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = Math.min(arr[i], arr[i - 1] + 1);
+        }
+        return arr[arr.length - 1];
+
+    }
+
+}
