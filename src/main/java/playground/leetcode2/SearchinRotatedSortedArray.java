f1e520b8d6874379e709e99f8d6f78e9598dfe8b
Liviu Stirb <liviu31@gmail.com>
2026-07-21 22:09:51 +0300

    leetcode problem

---
+++ src/main/java/playground/leetcode2/SearchinRotatedSortedArray.java
@@ -0,0 +1,30 @@
+package playground.leetcode2;
+
+public class SearchinRotatedSortedArray {
+
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[low] <= nums[mid]) {
+                // left half is sorted
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                // right half is sorted
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
