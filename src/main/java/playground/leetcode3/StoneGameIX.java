4c6973ddf5f43b19058f3e4c769976f94d174f91
Liviu Stirb <liviu31@gmail.com>
2026-08-16 22:37:51 +0300

    leetcode problem

---
+++ src/main/java/playground/leetcode3/StoneGameIX.java
@@ -0,0 +1,58 @@
+package playground.leetcode3;
+
+public class StoneGameIX {
+
+
+    private int[] cnt;
+
+    public boolean stoneGameIX(int[] stones) {
+        cnt = counts(stones);
+        return bk(true, 0, cnt[0], cnt[1], cnt[2]);
+    }
+
+    private boolean bk(boolean aliceTurn, int sumMod, int c0, int c1, int c2) {
+        if (c0 == 0 && c1 == 0 && c2 == 0) {
+            return false;
+        }
+
+        if (aliceTurn) {
+            boolean r0 = false, r1 = false, r2 = false;
+            if (c0 > 0) {
+                int newSum = (sumMod + 0) % 3;
+                r0 = (newSum == 0) ? false : bk(false, newSum, c0 - 1, c1, c2);
+            }
+            if (c1 > 0) {
+                int newSum = (sumMod + 1) % 3;
+                r1 = (newSum == 0) ? false : bk(false, newSum, c0, c1 - 1, c2);
+            }
+            if (c2 > 0) {
+                int newSum = (sumMod + 2) % 3;
+                r2 = (newSum == 0) ? false : bk(false, newSum, c0, c1, c2 - 1);
+            }
+            return r0 || r1 || r2;
+        } else {
+            boolean r0 = true, r1 = true, r2 = true;
+            if (c0 > 0) {
+                int newSum = (sumMod + 0) % 3;
+                r0 = (newSum == 0) ? true : bk(true, newSum, c0 - 1, c1, c2);
+            }
+            if (c1 > 0) {
+                int newSum = (sumMod + 1) % 3;
+                r1 = (newSum == 0) ? true : bk(true, newSum, c0, c1 - 1, c2);
+            }
+            if (c2 > 0) {
+                int newSum = (sumMod + 2) % 3;
+                r2 = (newSum == 0) ? true : bk(true, newSum, c0, c1, c2 - 1);
+            }
+            return r0 && r1 && r2;
+        }
+    }
+
+    private int[] counts(int[] stones) {
+        int cnt[] = new int[3];
+        for (int val : stones) {
+            cnt[val % 3]++;
+        }
+        return cnt;
+    }
+}
